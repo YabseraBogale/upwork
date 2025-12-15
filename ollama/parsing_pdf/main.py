025515ac8b4dc0eb7b0048a823b278bd97b088d5
YabseraBogale <yabserapython@gmail.com>
2025-12-15 17:30:22 +0300

    starting

--- ollama/parsing_pdf/main.py
+++ ollama/parsing_pdf/main.py
@@ -1,10 +1,21 @@
 
 from google import genai
-
+from pypdf import PdfReader
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client()
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
-)
-print(response.text)
+reader=PdfReader("./app.pdf")
+
+page=""
+
+for i in reader.pages:
+    page+=i.extract_text()
+try:
+
+    client = genai.Client()
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=f"can yout give the returns from the route as json in {page}"
+    )
+    print(response.text)
+except Exception as e:
+    print(e)
