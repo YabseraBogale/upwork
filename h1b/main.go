1c7e840e1983b258a8f083e1fb9cbd91b6115271
YabseraBogale <yabserapython@gmail.com>
2025-12-19 09:20:37 +0300

    h1b analysis

--- h1b/main.go
+++ h1b/main.go
@@ -1,1 +1,18 @@
+package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.ReadFile("/home/yabsera/Downloads/Alabama_H1B_Jobs.csv")
+	if err != nil {
+		log.Println(err)
+	}
+
+	for _, i := range file {
+		fmt.Println(i)
+	}
+}
