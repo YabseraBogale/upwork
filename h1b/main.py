2e717619bb27773c14c8efdb7b87e233e5139f3f
YabseraBogale <yabserapython@gmail.com>
2025-12-19 16:42:39 +0300

    h1b analysis

--- h1b/main.py
+++ h1b/main.py
@@ -4,4 +4,7 @@ df = pd.read_html("/home/yabsera/Documents/h1b_data/Alabama_H1B_Jobs.html")
 
 df = df[0]
 
-print(df.columns)
+df =df[df["h_1b_dependent"]=="No"]
+print("Number of h1b dependent saying NO is:",len(df))
+
+print(df["job_title"].unique)
